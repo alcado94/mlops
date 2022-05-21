8280c5c6aed7844da49ecf32304c80fec867a1cf
alejandro <alejandro_campos_dev@hotmail.com>
2022-05-21 15:45:57 +0200

    Fix train script

--- src/main/scripts/train.py
+++ src/main/scripts/train.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 import joblib
 from sklearn.decomposition import PCA
-
+from sklearn.model_selection import RandomizedSearchCV
 
 PCA_N_COMPONENTS = 4
 
@@ -63,7 +63,7 @@ def transformPCA(df, label):
     pca = PCA(n_components=PCA_N_COMPONENTS, svd_solver='full')
     df = pca.fit_transform(df)
 
-    with open("pca.txt", "w") as outfile:
+    with open("params.txt", "w") as outfile:
         outfile.write("\nInfo PCA: \n")
         outfile.write("N Compoments: " + str(PCA_N_COMPONENTS) + "\n")
         outfile.write("Explained Variance Ratio: " + str(pca.explained_variance_ratio_) + "\n")
@@ -90,13 +90,49 @@ df = (
             transformPCA(_df.drop("congestion",axis=1), _df['congestion'])
         )
 )
-
+print("Finished preprocessing")
 
 X_train, X_test, y_train, y_test = train_test_split(df.drop("congestion",axis=1), df['congestion'], test_size=0.33, random_state=42)
+
+
+print("Training...")
 # Fit a model
-clf = RandomForestRegressor(n_estimators=100)
+#
+# Number of trees in random forest
+n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 5)]
+# Number of features to consider at every split
+max_features = ['auto', 'sqrt']
+# Maximum number of levels in tree
+max_depth = [int(x) for x in np.linspace(10, 110, num = 4)]
+max_depth.append(None)
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 4]
+# Method of selecting samples for training each tree
+bootstrap = [True, False]
+
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
+
+
+# Use the random grid to search for best hyperparameters
+# First create the base model to tune
+rf = RandomForestRegressor()
+# Random search of parameters, using 3 fold cross validation, 
+# search across 100 different combinations, and use all available cores
+clf = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+# Fit the random search model
+
+# clf = RandomForestRegressor(n_estimators=100, n_jobs=-1, random_state = 42)
 
 clf.fit(X_train, y_train)
+
+print("Finished training")
 
 y_pred = clf.predict(X_test)
 
